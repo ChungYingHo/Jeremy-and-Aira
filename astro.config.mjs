96cf9f203e3111dc78c208b21b1c0912885828ed
Jeremy <ag.cyho@gmail.com>
2026-02-02 20:23:58 +0800

    chore: add tailwind & scss

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,19 @@ import { defineConfig } from 'astro/config';
 
 import svelte from '@astrojs/svelte';
 
+import tailwindcss from '@tailwindcss/vite';
+import path from 'node:path'
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte()]
+  integrations: [svelte()],
+
+  vite: {
+    resolve: {
+      alias: {
+        '@': path.resolve('./src'),
+      },
+    },
+    plugins: [tailwindcss()]
+  }
 });
