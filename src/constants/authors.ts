e2d939e696edb2edc4858a030f4580014e7148e3
Jeremy <ag.cyho@gmail.com>
2026-02-09 20:59:45 +0800

    feat: add authors config

---
+++ src/constants/authors.ts
@@ -0,0 +1,23 @@
+export const AUTHORS = [
+  {
+    name: 'Jeremy',
+    imgPath: '/assets/authors/jeremy.png',
+    summary: 'A passionate web developer and tech enthusiast.',
+    description: 'Jeremy has over 10 years of experience in web development, specializing in frontend technologies and user experience design. He loves sharing knowledge through blogging and open-source contributions.',
+    currentRole: [
+      'Frontend Engineer at TechCorp',
+      'Open Source Contributor',
+      'Tech Blogger at Medium'
+    ],
+    socialLinks: {
+      github: '',
+      linkedin: '',
+      npm: '',
+    }
+  },
+  {
+    name: 'Aira',
+    imgPath: '/assets/authors/aira.png',
+    summary: 'A creative designer and full-stack developer.',
+  }
+]
